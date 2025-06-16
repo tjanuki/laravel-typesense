7276feaf8de914fca45132e06e0334ed91f3e7b7
tjanuki <t.januki@gmail.com>
2025-06-16 19:41:27 -0400

    Create Typesense client and import books documents

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Typesense\Client;
 
-Route::get('/', function () {
+Route::get('/create-collection', function () {
     $client = new Client([
         'api_key' => config('services.typesense.api_key'),
         'nodes' => [
@@ -20,7 +20,7 @@ Route::get('/', function () {
         'name' => 'books',
         'fields' => [
             ['name' => 'title', 'type' => 'string'],
-            ['name' => 'author', 'type' => 'string[]'],
+            ['name' => 'authors', 'type' => 'string[]'],
             ['name' => 'publication_year', 'type' => 'int32'],
             ['name' => 'ratings_count', 'type' => 'int32'],
             ['name' => 'average_rating', 'type' => 'float'],
@@ -28,12 +28,55 @@ Route::get('/', function () {
         'default_sorting_field' => 'ratings_count',
     ];
 
-//    $client->collections->create($bookSchema);
+    $client->collections['books']->delete();
+
+    $client->collections->create($bookSchema);
+
+    return 'Collection created';
+});
+
+Route::get('/import-collection', function () {
+    $client = new Client([
+        'api_key' => config('services.typesense.api_key'),
+        'nodes' => [
+            [
+                'host' => config('services.typesense.host'),
+                'port' => config('services.typesense.port'),
+                'protocol' => config('services.typesense.protocol'),
+            ],
+        ],
+        'connection_timeout_seconds' => 2,
+    ]);
 
     $books = file_get_contents(base_path('books.jsonl'));
 
-    $client->collections['books']->documents->import($books);
-
+    $results = $client->collections['books']->documents->import($books);
 
     return 'Books imported';
+});
+
+Route::get('/search-collection', function () {
+    $client = new Client([
+        'api_key' => config('services.typesense.api_key'),
+        'nodes' => [
+            [
+                'host' => config('services.typesense.host'),
+                'port' => config('services.typesense.port'),
+                'protocol' => config('services.typesense.protocol'),
+            ],
+        ],
+        'connection_timeout_seconds' => 2,
+    ]);
+
+    $books = file_get_contents(base_path('books.jsonl'));
+
+    $results = $client->collections['books']->documents->search([
+       'q' => request('q'),
+        'query_by' => 'title,authors',
+        'sort_by' => '_text_match:desc,publication_year:desc',
+    ]);
+
+    $titles = collect($results['hits'])->map(fn($hit) => $hit['document']['title']);
+
+    return $titles;
 });
