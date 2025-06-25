219ba6916eb0b5d275560b719aa76abc90556bff
tjanuki <t.januki@gmail.com>
2025-06-25 16:10:16 -0400

    Add search page and route

---
+++ resources/views/search.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Search</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="p-8 max-w-lg">
+
+<form action="/search" method="GET">
+    <input
+        type="search"
+        name="q"
+        class="border border-b-gray-300 border-b-0 w-full py-2 px-4"
+        value="{{request('q')}}"
+    >
+</form>
+
+<ul class="border border-gray-300 py-2 px-4">
+    @forelse($results as $result)
+        <li>{{ $result }}</li>
+    @empty
+        <li>No matching result</li>
+    @endforelse
+</ul>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,75 +3,90 @@
 use Illuminate\Support\Facades\Route;
 use Typesense\Client;
 
-Route::get('/create-collection', function (Client $client) {
+//Route::get('/create-collection', function (Client $client) {
+//
+//    $bookSchema = [
+//        'name' => 'books',
+//        'fields' => [
+//            ['name' => 'title', 'type' => 'string'],
+//            ['name' => 'authors', 'type' => 'string[]', 'facet' => true],
+//            ['name' => 'publication_year', 'type' => 'int32'],
+//            ['name' => 'ratings_count', 'type' => 'int32'],
+//            ['name' => 'average_rating', 'type' => 'float'],
+//        ],
+//        'default_sorting_field' => 'ratings_count',
+//    ];
+//
+//    $client->collections['books']->delete();
+//
+//    $client->collections->create($bookSchema);
+//
+//    return 'Collection created';
+//});
+//
+//Route::get('/import-collection', function (Client $client) {
+//
+//    $books = file_get_contents(base_path('books.jsonl'));
+//
+//    $results = $client->collections['books']->documents->import($books);
+//
+//    return 'Books imported';
+//});
+//
+//Route::get('/search-collection', function (Client $client) {
+//
+//    $books = file_get_contents(base_path('books.jsonl'));
+//
+//    $results = $client->collections['books']->documents->search([
+//       'q' => request('q'),
+//        'query_by' => 'title,authors',
+//        'sort_by' => '_text_match:desc,publication_year:desc',
+//    ]);
+//
+//    $titles = collect($results['hits'])->map(fn($hit) => $hit['document']['title']);
+//
+//    return $titles;
+//});
+//
+//Route::get('/filter-search', function (Client $client) {
+//
+//    $books = file_get_contents(base_path('books.jsonl'));
+//
+//    $results = $client->collections['books']->documents->search([
+//        'q' => request('q'),
+//        'query_by' => 'title',
+//        'sort_by' => '_text_match:desc,ratings_count:desc',
+//        'per_page' => 50,
+//        'filter_by' => 'publication_year:[1990..2000, 2010..2020]',
+//    ]);
+//
+//    return $results;
+//});
+//
+//Route::get('/faceting', function (Client $client) {
+//
+//    $results = $client->collections['books']->documents->search([
+//        'q' => request('q'),
+//        'query_by' => 'title',
+//        'sort_by' => '_text_match:desc,ratings_count:desc',
+//        'per_page' => 50,
+//        'facet_by' => 'authors',
+//    ]);
+//
+//    return $results;
+//});
 
-    $bookSchema = [
-        'name' => 'books',
-        'fields' => [
-            ['name' => 'title', 'type' => 'string'],
-            ['name' => 'authors', 'type' => 'string[]', 'facet' => true],
-            ['name' => 'publication_year', 'type' => 'int32'],
-            ['name' => 'ratings_count', 'type' => 'int32'],
-            ['name' => 'average_rating', 'type' => 'float'],
-        ],
-        'default_sorting_field' => 'ratings_count',
-    ];
-
-    $client->collections['books']->delete();
-
-    $client->collections->create($bookSchema);
-
-    return 'Collection created';
-});
-
-Route::get('/import-collection', function (Client $client) {
-
-    $books = file_get_contents(base_path('books.jsonl'));
-
-    $results = $client->collections['books']->documents->import($books);
-
-    return 'Books imported';
-});
-
-Route::get('/search-collection', function (Client $client) {
-
-    $books = file_get_contents(base_path('books.jsonl'));
+Route::get('/search', function (Client $client) {
+    $query = request('q', '*');
 
     $results = $client->collections['books']->documents->search([
-       'q' => request('q'),
-        'query_by' => 'title,authors',
-        'sort_by' => '_text_match:desc,publication_year:desc',
-    ]);
-
-    $titles = collect($results['hits'])->map(fn($hit) => $hit['document']['title']);
-
-    return $titles;
-});
-
-Route::get('/filter-search', function (Client $client) {
-
-    $books = file_get_contents(base_path('books.jsonl'));
-
-    $results = $client->collections['books']->documents->search([
-        'q' => request('q'),
+        'q' => $query,
         'query_by' => 'title',
-        'sort_by' => '_text_match:desc,ratings_count:desc',
-        'per_page' => 50,
-        'filter_by' => 'publication_year:[1990..2000, 2010..2020]',
     ]);
 
-    return $results;
-});
+    $results = collect($results['hits'])->pluck('document.title');
 
-Route::get('/faceting', function (Client $client) {
-
-    $results = $client->collections['books']->documents->search([
-        'q' => request('q'),
-        'query_by' => 'title',
-        'sort_by' => '_text_match:desc,ratings_count:desc',
-        'per_page' => 50,
-        'facet_by' => 'authors',
+    return view('search', [
+        'results' => $results,
     ]);
-
-    return $results;
 });
